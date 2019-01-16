707e71c201a4337c7c7736f04cad5783646ecb00
Eric Hernandez <eands9@yahoo.com>
2019-01-16 07:06:57 -0600

    newworld4

--- SpellingBee-4th/Model/WordBank.swift
+++ SpellingBee-4th/Model/WordBank.swift
@@ -12,15 +12,15 @@ class WordBank{
     var list = [Word]()
     
     init(){
-        list.append(Word(word:"cashew", sentence:""))
-        list.append(Word(word:"luau", sentence:""))
-        list.append(Word(word:"totem", sentence:""))
-        list.append(Word(word:"mahimahi", sentence:""))
-        list.append(Word(word:"hickory", sentence:""))
-        list.append(Word(word:"cacao", sentence:""))
-        list.append(Word(word:"kona", sentence:""))
-        list.append(Word(word:"malihini", sentence:""))
-        list.append(Word(word:"wikiwiki", sentence:""))
-        list.append(Word(word:"tuckahoe", sentence:""))
+        list.append(Word(word:"pecan", sentence:""))
+        list.append(Word(word:"chipotle", sentence:""))
+        list.append(Word(word:"skunk", sentence:""))
+        list.append(Word(word:"woodchuck", sentence:""))
+        list.append(Word(word:"chocolate", sentence:""))
+        list.append(Word(word:"muumuu", sentence:""))
+        list.append(Word(word:"puma", sentence:""))
+        list.append(Word(word:"tomato", sentence:""))
+        list.append(Word(word:"maraca", sentence:""))
+        list.append(Word(word:"petunia", sentence:""))
     }
 }
